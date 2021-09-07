ca21f3bb1c2bb58fe1fb788cd8d7d3fd16502b17
rengaoli <rgl15195652554@163.com>
2021-09-07 10:26:57 +0800

    first commit

---
+++ src/views/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+      
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
